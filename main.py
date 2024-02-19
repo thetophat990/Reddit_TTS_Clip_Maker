605e11e6cb65baff3f162abb2d2793e8fbb94e40
Richard Coleman <39260846+thetophat990@users.noreply.github.com>
2024-02-19 20:08:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
+from open_ai_services import genarate_subtitles, genarate_tts
+from reddit_api_services import get_comment_string
 from Video_Maker import make_video
+import SETTINGS
 
-from random import randint
 from os import listdir
 from os.path import join
+from random import randint
+from datetime import datetime
 
-MOCK_DATA = """mock data, mock data, my name is walter hartwell whight and i live at 305 negra aroya lane albuqurci new mexio"""
-TEMP_AUDIO_PATH = r"audio.mp3"
-OUTPUT_PATH = r"output.mp4"
-GAMEPLAY_FOLDER_PATH = r"backround_video"
-OUTPUT_FPS = 60
+def rand_video_clip(path_to_backround_videos):
+    backround_videos_list = listdir(path_to_backround_videos)
+    random_video = backround_videos_list[randint(0, len(backround_videos_list) - 1)]
+    random_video_path = join(path_to_backround_videos, random_video)
+    return random_video_path
 
-rand_gameplay_path = join(GAMEPLAY_FOLDER_PATH, listdir(GAMEPLAY_FOLDER_PATH)[randint(0, len(listdir(GAMEPLAY_FOLDER_PATH)) - 1)])
+output_filename = datetime.now().strftime(SETTINGS.GENERAL_OUTPUT_FILENAME)
+output_path = join(SETTINGS.MOVIEPY_VIDEO_OUTPUT_DIRECTORY, output_filename)
 
-# UPDATE FOR NEW TTS CODEBASE
+reddit_comments = get_comment_string(
+    SETTINGS.REDDIT_API_USER_AGENT,
+    SETTINGS.REDDIT_API_CLIENT_ID,
+    SETTINGS.REDDIT_API_CLIENT_SECRET,
+    SETTINGS.REDDIT_API_SUBREDDIT_LISTS,
+    SETTINGS.REDDIT_API_MAX_POSTS_TO_DOWNLOAD,
+    SETTINGS.REDDIT_API_MIN_CHARS_PER_POST
+    )
 
-make_video(OUTPUT_PATH,rand_gameplay_path,OUTPUT_FPS,randint(0,25),TEMP_AUDIO_PATH)
+with open(SETTINGS.OPENAI_AUDIO_OUTPUT_PATH, "wb") as file_object:
+    tts_audio = genarate_tts(reddit_comments, SETTINGS.OPENAI_API_KEY, SETTINGS.OPENAI_TTS_VOICE, SETTINGS.OEPNAI_MAX_CHARS_FOR_REQUEST)
+    file_object.write(tts_audio)
+    
+with open(SETTINGS.OPENAI_CAPTION_OUTPUT_PATH, "w") as file_object:
+    caption_data = genarate_subtitles(SETTINGS.OPENAI_API_KEY, SETTINGS.OPENAI_AUDIO_OUTPUT_PATH)
+    file_object.write(repr(caption_data))
+    
+make_video(output_path,
+           rand_video_clip(SETTINGS.MOVIEPY_PATH_TO_BACKROUND_VIDEOS),
+           SETTINGS.OPENAI_AUDIO_OUTPUT_PATH,
+           SETTINGS.OPENAI_CAPTION_OUTPUT_PATH,
+           SETTINGS.MOVIEPY_AUDIO_CODEC,
+           SETTINGS.MOVIEPY_VIDEO_CODEC,
+           SETTINGS.MOVIEPY_MOVIEPY_OUTPUT_FPS,
+           SETTINGS.MOVIEPY_GAMEPLAY_START_OFFSET,
+           SETTINGS.MOVIEPY_VIDEO_DIMENSIONS)
+
+
+
