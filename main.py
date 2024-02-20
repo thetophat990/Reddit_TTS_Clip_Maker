e98131a9642c4a7d8176dc789fab404439449fbf
Richard Coleman <39260846+thetophat990@users.noreply.github.com>
2024-02-20 09:21:18 +0000

    Update main.py
    Added comments and type hinting to "rand_video_clip" function

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from open_ai_services import genarate_subtitles, genarate_tts
-from reddit_api_services import get_comment_string
-from Video_Maker import make_video
+from open_ai_services import genarate_subtitles, genarate_tts    # Import OpenAI API Call Functions
+from reddit_api_services import get_comment_string               # Import Reddit API Call (via PRAW) Functions
+from Video_Maker import make_video                               # Import Video Generation Functions
 import SETTINGS
 
-from os import listdir
-from os.path import join
-from random import randint
-from datetime import datetime
+from os import listdir                                           # For Listing Contense of backround_video dir
+from os.path import join                                         # For concatinating file names and paths
+from random import randint                                       # For Selection Of Random Backround Videos
+from datetime import datetime                                    # For Adding Date Time Info In Output File Filename
 
-def rand_video_clip(path_to_backround_videos):
+def rand_video_clip(path_to_backround_videos : str) -> str:                    
     backround_videos_list = listdir(path_to_backround_videos)
     random_video = backround_videos_list[randint(0, len(backround_videos_list) - 1)]
     random_video_path = join(path_to_backround_videos, random_video)
