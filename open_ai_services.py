85a1c8fa71fa6bd0904b9e18f662a0a841b8d3f7
Richard Coleman <39260846+thetophat990@users.noreply.github.com>
2024-02-20 11:59:45 +0000

    Update open_ai_services.py

--- open_ai_services.py
+++ open_ai_services.py
@@ -1,48 +1,48 @@
-from openai import OpenAI
-from json import loads
+from openai import OpenAI        # Used to make the OpenAI API calls
+from json import loads           # Used to load response of subtitles into python dictonery
 
 def genarate_subtitles(api_key : str, audio_path : str = "audio.mp3") -> list[dict[float, str]]:
     return_list = []
     
-    api_object = OpenAI(api_key=api_key)
+    api_object = OpenAI(api_key=api_key)    # Create openAI API object
     
-    with open(audio_path, "rb") as file_obj:
-        response = api_object.audio.transcriptions.create(
+    with open(audio_path, "rb") as file_obj:    # Read audio data from TTS output
+        response = api_object.audio.transcriptions.create(    # Call transcription model
                 file=file_obj,
-                model="whisper-1",
+                model="whisper-1",    # TODO change this to a setting instead of hard coded
                 response_format="verbose_json"
         )
    
-    data = loads(response.model_dump_json())["segments"]
+    data = loads(response.model_dump_json())["segments"]    # Loads segmants section into python
     
     for subtitles in data:
-        words = subtitles['text'].split(' ')
-        duration = subtitles['end'] - subtitles['start']
-        time_buffer = subtitles['start']
+        words = subtitles['text'].split(' ')    # Split the sentance into individual words
+        duration = subtitles['end'] - subtitles['start']    # Calculate duration
+        time_buffer = subtitles['start']    # Buffer used to store last time, initalised at start of subtitles
         for word in words:
-            if word in ['', ' ', '.', ',', '\n']: continue
-            calc_speak_time = (len(word) / len(subtitles['text'])) * duration
+            if word in ['', ' ', '.', ',', '\n']: continue    # Remove non words, e.g empty strings
+            calc_speak_time = (len(word) / len(subtitles['text'])) * duration    # calculate time to speak for each word
             
-            return_list.append({'start' : round(time_buffer, 2), 'end' : round(time_buffer + calc_speak_time, 2), 'text' : word})
+            return_list.append({'start' : round(time_buffer, 2), 'end' : round(time_buffer + calc_speak_time, 2), 'text' : word}) # Append new subitle data to list, and rounding values
             time_buffer += calc_speak_time
 
     return return_list
 
 def genarate_tts(text : str, api_key : str, voice : str = "alloy", max_chars : int = 4095) -> bytes:
-    api_object = OpenAI(api_key=api_key)
+    api_object = OpenAI(api_key=api_key)    # Create openAI API object
     
-    if len(text) >= max_chars:
+    if len(text) >= max_chars:              # Check if charicters in string exceed maximum charicter length, if so, trim the string 
         text = text[:max_chars]
     
-    response = api_object.audio.speech.create(
+    response = api_object.audio.speech.create(    # Call TTS Model
         input=text,
         model="tts-1",
         voice=voice
     )
       
-    response = response.read()
+    response = response.read()    # Decode response into audio format
         
     return response
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # TODO add module test
     pass
