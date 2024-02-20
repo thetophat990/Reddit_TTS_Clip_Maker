9e2847d56ada9b4d615cd4d908329a2a4ee815d8
Richard Coleman <39260846+thetophat990@users.noreply.github.com>
2024-02-20 09:52:23 +0000

    Update SETTINGS.py

--- SETTINGS.py
+++ SETTINGS.py
@@ -12,18 +12,18 @@ REDDIT_API_MIN_CHARS_PER_POST = 50                                 # Minimum cha
 
 # OPEN-AI SETTINGS
 OPENAI_API_KEY = r"CHANGEME"                                       # Update this with your OpenAI API key, more info on how to do this can be found on main README
-OPENAI_AUDIO_OUTPUT_PATH = r"working_dir\audio.mp3"
-OPENAI_CAPTION_OUTPUT_PATH = r"working_dir\caption_data"
-OPENAI_TTS_VOICE = r"alloy"
-OEPNAI_MAX_CHARS_FOR_REQUEST = 4095
+OPENAI_AUDIO_OUTPUT_PATH = r"working_dir\audio.mp3"                # The path for the tts audio files to be saved to
+OPENAI_CAPTION_OUTPUT_PATH = r"working_dir\caption_data"           # The path for the reddit comment strings to be saved to
+OPENAI_TTS_VOICE = r"alloy"                                        # The voice of the OpenAI TTS, see https://platform.openai.com/docs/guides/text-to-speech/voice-options for all voices
+OEPNAI_MAX_CHARS_FOR_REQUEST = 4095                                # Max charicters for the video, OpenAI has a hard limit of 4096 chars per tts request
 
 #MOVIEPY SETTINGS
-MOVIEPY_PATH_TO_BACKROUND_VIDEOS = r"backround_video"
-MOVIEPY_VIDEO_OUTPUT_DIRECTORY = r"output"
-MOVIEPY_VIDEO_CODEC = r"libx264"
-MOVIEPY_AUDIO_CODEC = r"aac"
-MOVIEPY_MOVIEPY_OUTPUT_FPS = 30
-MOVIEPY_GAMEPLAY_START_OFFSET = 5
-MOVIEPY_VIDEO_DIMENSIONS = 405, 720
+MOVIEPY_PATH_TO_BACKROUND_VIDEOS = r"backround_video"              # Path to directory containing backround video files
+MOVIEPY_VIDEO_OUTPUT_DIRECTORY = r"output"                         # Directory for generated videos to be saved to
+MOVIEPY_VIDEO_CODEC = r"libx264"                                   # Video codec to use for generated video
+MOVIEPY_AUDIO_CODEC = r"aac"                                       # Audio codec to use for generated video
+MOVIEPY_MOVIEPY_OUTPUT_FPS = 30                                    # Frames per second of generated video
+MOVIEPY_GAMEPLAY_START_OFFSET = 5                                  # Seconds to offset the start of the backround gameplay, to avoid video intros ect
+MOVIEPY_VIDEO_DIMENSIONS = 405, 720                                # Video dimensions, fomat is "width, height"
 
 
